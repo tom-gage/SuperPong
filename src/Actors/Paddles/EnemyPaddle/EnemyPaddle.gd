a54bbc376b9d4dbac7aaab4e0bf633041e0d219b
Tom Gage <tomgage01@gmail.com>
2020-08-18 00:41:37 +0100

    added a bit of smoooothe to the player controller, I'll investigate how it works tommorow

--- src/Actors/Paddles/EnemyPaddle/EnemyPaddle.gd
+++ src/Actors/Paddles/EnemyPaddle/EnemyPaddle.gd
@@ -1,14 +1,49 @@
 extends Actor
 
 var ball_position = Vector2.ZERO
+var desired_position = 0
+var direction = Vector2.ZERO
 
+func _ready() -> void:
+	speed = Vector2(100, -100)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position.y = ball_position.y
+	#position.y = ball_position.y
+	pass
+
+func _physics_process(delta: float) -> void:
+	var direction = get_movement_direction()
+	_velocity = calculate_move_velocity(_velocity, direction, speed)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	
+	#keep paddle on-screen
+	position.y = clamp(position.y, 0 + 79, 720 - 79)
+
+
+
+func calculate_move_velocity(
+	linear_velocity: Vector2,
+	direction: Vector2,
+	speed: Vector2
+	) -> Vector2:
+		var move_velocity = linear_velocity
+		
+		move_velocity.x = speed.x * direction.x
+		move_velocity.y = speed.y * direction.y
+		
+		return move_velocity
+
+func get_movement_direction()->Vector2:
+	if(ball_position.y < position.y):
+		return Vector2(0, 1)
+	else:
+		return Vector2(0, -1)
 
 func get_ball_position(balls_current_position: Vector2) -> void:
 	ball_position = balls_current_position
 
 func reset_position()->void:
 	position = Vector2(1055, 360)
+
+func go_to_range(target_pos: Vector2, difficulty_multiplier: int)->void:
+	pass
--- src/Actors/Paddles/PlayerPaddle/PlayerPaddle.gd
+++ src/Actors/Paddles/PlayerPaddle/PlayerPaddle.gd
@@ -1,8 +1,10 @@
 extends Actor
 
-# Called when the node enters the scene tree for the first time.
+var current_speed = Vector2.ZERO
+
 func _ready() -> void:
-	speed = Vector2(200, -200)
+	speed = Vector2(400, -400)
+
 
 func _physics_process(delta: float) -> void:
 	var direction = get_movement_direction()
@@ -12,23 +14,22 @@ func _physics_process(delta: float) -> void:
 	#keep paddle on-screen
 	position.y = clamp(position.y, 0 + 79, 720 - 79)
 
+
 func get_movement_direction() -> Vector2:
 	return Vector2(
 		0,
 		Input.get_action_strength("paddle_up") - Input.get_action_strength("paddle_down")
 	)
 
-func calculate_move_velocity(
-	linear_velocity: Vector2,
-	direction: Vector2,
-	speed: Vector2
-	) -> Vector2:
+func calculate_move_velocity(linear_velocity: Vector2, direction: Vector2, speed: Vector2) -> Vector2:
 		var move_velocity = linear_velocity
 		
-		move_velocity.x = speed.x * direction.x
-		move_velocity.y = speed.y * direction.y
+		move_velocity = speed * direction
 		
-		return move_velocity
+		current_speed = lerp(current_speed, move_velocity, 0.2)
+		return current_speed
+		
+		#return move_velocity
 
 func reset_position()->void:
 	position = Vector2(25, 360)
